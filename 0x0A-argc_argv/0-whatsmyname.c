91210a2993884dd1972833c5390341f7be433a52
Adoy312 <gabriel.kyeremateng.gk@gmail.com>
2022-04-04 09:04:57 +0000

    Hot fix

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,14 +2,14 @@
 
 /**
  * main - prints program name
- * argc: number of arguments
- * argv: array of pointers
+ * @argc: number of arguments
+ * @argv: array of pointers
  * Return: 0
  */
 
-int main(int argc __attribute__((unused)), char)
+int main(int argc __attribute__((unused), char **argv)
 {
-    printf("%s\n", *(argv + 0));
-    return (0);
+   printf("%s\n", *(argv + 0));
+   return (0);
 
 }
